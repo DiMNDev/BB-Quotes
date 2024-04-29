3615e5ada87943ccb199db40babab8ce5f7d7174
DiMNDev <Dimndev@gmail.com>
2024-04-29 15:58:10 -0600

    add button and custom colors

--- BB Quotes/QuoteView.swift
+++ BB Quotes/QuoteView.swift
@@ -15,7 +15,9 @@ struct QuoteView: View {
                     .resizable()
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                 VStack {
+                    Spacer(minLength: 150)
                     Text("\"You either run from things, or you face them, Mr. White.\"")
+                        .minimumScaleFactor(0.5)
                         .multilineTextAlignment(.center)
                         .foregroundStyle(.white)
                         .padding()
@@ -35,6 +37,18 @@ struct QuoteView: View {
                     }
                     .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
                     .clipShape(.rect(cornerRadius: 80))
+                    Spacer()
+                    Button {}
+                        label: {
+                            Text("Get Random Quote")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.breakingBadGreen)
+                                .clipShape(.rect(cornerRadius: 12))
+                                .shadow(color: .breakingBadYellow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        }
+                    Spacer(minLength: 180)
                 }
                 .frame(width: geo.size.width)
             }

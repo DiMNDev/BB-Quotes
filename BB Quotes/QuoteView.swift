5be3a9c64c1fac9f22f90d228f0975dcc9e2bc1d
DiMNDev <Dimndev@gmail.com>
2024-04-30 12:45:01 -0600

    integrate detail view

--- BB Quotes/QuoteView.swift
+++ BB Quotes/QuoteView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct QuoteView: View {
     @StateObject private var viewModel = ViewModel(controller: FetchController())
+    @State private var detail: Bool = false
     let show: String
     var body: some View {
         GeometryReader { geo in
@@ -41,6 +42,12 @@ struct QuoteView: View {
                                 ProgressView()
                             }
                             .frame(width: geo.size.width/1.1, height: geo.size.height/1.8)
+                            .onTapGesture {
+                                detail.toggle()
+                            }
+                            .sheet(isPresented: $detail, content: {
+                                CharacterView(show: show, character: data.character)
+                            })
                             
                             Text("\(data.quote.character)")
                                 .foregroundStyle(.white)

d0f64826a3061976c563a268a3be0575c1fa7c4e
DiMNDev <Dimndev@gmail.com>
2024-04-30 09:56:22 -0600

    add Constants and add String extensions

--- BB Quotes/Constants.swift
+++ BB Quotes/Constants.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
+enum Constants {
+    static let bbName = "Breaking Bad"
+    static let bcsName = "Better Call Saul"
+}
+
 extension String {
     var replaceSpaceWithPlus: String {
         self.replacingOccurrences(of: " ", with: "+")
+    }
+
+    var trimWhiteSpace: String {
+        self.replacingOccurrences(of: " ", with: "")
+    }
+
+    var lowerNoSpaces: String {
+        self.trimWhiteSpace.lowercased()
     }
 }
